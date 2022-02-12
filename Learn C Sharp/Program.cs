7146559b4f8b041401089b7a87aed05f68d4de80
Kameer Hosein <kameer-hosien@outlook.com>
2022-02-12 09:37:17 -0400

    Add project files.

---
+++ Learn C Sharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Learn_C_Sharp;
+
+Square smallsquare=new Square();
+smallsquare.Side = 1;
+
+Square bigsquare=new Square();
+bigsquare.Side = 600;
+
+Console.Beep();
+Console.WriteLine($"The size of the small square is {smallsquare.Side}");
+Console.WriteLine($"The size of the big square is {bigsquare.Side}");
---
+++ Learn C Sharp/Square.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_C_Sharp
+{
+    public class Square
+    {
+        public int Side { get; set; }
+
+    }
+}
